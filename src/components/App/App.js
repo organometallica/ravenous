dbdda05bd944d73221d3793a71169282ee33ee83
organometallica <marshall.r.brennan@gmail.com>
2020-05-07 22:26:29 -0500

    Data is still hard-coded, but now generates the page dynamically.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import './App.css';
+import Business from '../Business/Business';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+let businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+  ]
 
 
 function App() {
@@ -9,7 +31,7 @@ function App() {
     <div className="App">
       <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList businesses={businesses} />
     </div>
   );
 }
