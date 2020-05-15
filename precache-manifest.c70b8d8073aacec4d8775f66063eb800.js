5f637d01991159126810b7970a748921b052d8f3
organometallica <marshall.r.brennan@gmail.com>
2020-05-15 17:17:16 -0500

    Updates

---
+++ precache-manifest.c70b8d8073aacec4d8775f66063eb800.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43478960134db43106444fd64ed42d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54d7f14329198450e4ee",
+    "url": "/static/css/main.074b7ff6.chunk.css"
+  },
+  {
+    "revision": "5b631a7ba56a5f56da04",
+    "url": "/static/js/2.70a0e2c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70a0e2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d7f14329198450e4ee",
+    "url": "/static/js/main.0b48a00d.chunk.js"
+  },
+  {
+    "revision": "2584094236ad393d10a5",
+    "url": "/static/js/runtime-main.64520233.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
